e828b02e0129c3070b8a83ed63ed26784b876586
Erick Ventura <erick94111@gmail.com>
2020-05-20 12:36:51 -0600

    adding dotenv enviroment file support

---
+++ src/interfaces/IDatabaseSetting.ts
@@ -0,0 +1,7 @@
+export default interface IDatabaseSetting {
+    host: string;
+    port: number;
+    database: string;
+    user: string;
+    password: string;
+}
---
+++ src/settings.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import IDatabaseSetting from './interfaces/IDatabaseSetting';
+
+dotenv.config();
+
+export const databaseSettings: IDatabaseSetting = {
+    host: String(process.env.HOST),
+    port: Number(process.env.PORT),
+    database: String(process.env.DATABASE),
+    user: String(process.env.USER),
+    password: String(process.env.PASSWORD),
+};
