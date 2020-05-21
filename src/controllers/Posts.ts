7b2cef8d971d765923c523729d521750aba58f57
Erick Ventura <erick94111@gmail.com>
2020-05-21 14:54:48 -0600

    get and update post

--- src/controllers/Posts.ts
+++ src/controllers/Posts.ts
@@ -1,9 +1,10 @@
-import { ServerResponse, IncomingMessage } from 'http';
+import { ServerResponse, IncomingMessage, Server } from 'http';
 import { QueryResult, Query } from 'pg';
 import client from '../db/db';
 import { RESPONSE_CODES } from '../enums/responseCodes';
 import { response } from '../router/response';
 import { parseData } from '../helpers/parseData';
+import { UrlWithParsedQuery, parse } from 'url';
 
 export const getPosts = async (res: ServerResponse) => {
     try {
@@ -11,17 +12,55 @@ export const getPosts = async (res: ServerResponse) => {
             'select * from posts where deleted=false order by created_at desc'
         );
 
-        response(res, 200, undefined, JSON.stringify(result.rows));
+        response(
+            res,
+            RESPONSE_CODES.OK,
+            undefined,
+            JSON.stringify(result.rows)
+        );
     } catch {
         response(res, 500);
     }
 };
 
 export const createPost = async (res: ServerResponse, req: IncomingMessage) => {
-    console.log(req);
     let data = await parseData(req);
     let result: QueryResult = await client.query(
         `insert into posts values(default,'${data.title}','${data.content}','${data.description}',default,default,null)`
     );
     response(res, RESPONSE_CODES.CREATED, 'Post created sucessfully');
+};
+
+export const getPost = async (res: ServerResponse, req: IncomingMessage) => {
+    let result: QueryResult = await client.query(
+        `select * from posts where id=${getId(req)}`
+    );
+    response(res, RESPONSE_CODES.OK, undefined, JSON.stringify(result.rows));
+};
+
+export const updatePost = async (res: ServerResponse, req: IncomingMessage) => {
+    let data = await parseData(req);
+    const id: number = getId(req);
+    let result: QueryResult = await client.query(
+        `update  posts set title='${data.title}', 
+                    content='${data.content}',
+                    description='${data.description}',
+                    updated_at=now()
+                    where id=${id}
+                    `
+    );
+
+    response(res, RESPONSE_CODES.OK, `Post id: ${id} updated sucessfully`);
+};
+
+const getId = (req: IncomingMessage) => {
+    const urlParsed: UrlWithParsedQuery = parse(String(req.url), true);
+    const path: string = String(urlParsed.path);
+    let id: number = Number(path.split('/posts/')[1]);
+    if (Number.isNaN(id)) {
+        const includes_extra = path.split('/posts/')[1];
+        id = Number(includes_extra.split('/')[0]);
+    }
+
+    return id;
 };
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,14 @@
 import { IRoute } from '../interfaces/IRoute';
-import { getPosts, createPost } from '../controllers/Posts';
+import {
+    getPosts,
+    createPost,
+    getPost,
+    updatePost,
+} from '../controllers/Posts';
 // list of available routes on api
 export const routes: IRoute[] = [
     { regex: /^\/posts\/?$/, method: 'GET', controller: getPosts },
     { regex: /^\/posts\/?$/, method: 'POST', controller: createPost },
+    { regex: /^\/posts\/([0-9]+)\/?$/, method: 'GET', controller: getPost },
+    { regex: /^\/posts\/([0-9]+)\/?$/, method: 'PUT', controller: updatePost },
 ];
